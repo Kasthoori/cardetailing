e9bcb3edde02198acf16cc71d33d706eb189a43f
Kasthoori <ap.kasth@gmail.com>
2025-05-04 18:38:11 +1200

    added seperate styles

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,7 +4,6 @@ import NavBar from './components/AppBarComponent/NavBar';
 const App:FC = () => {
   return (
     <div>
-      <h2 className='text-3xl font-bold'>Build Car Detailing</h2>
       <NavBar />
     </div>
   )
