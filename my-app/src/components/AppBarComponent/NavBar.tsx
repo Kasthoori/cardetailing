0da5fda7c3e7f091d8f5d75b9baad6760d4a207b
Kasthoori <ap.kasth@gmail.com>
2025-05-04 18:15:47 +1200

    add navbar component

---
+++ my-app/src/components/AppBarComponent/NavBar.tsx
@@ -0,0 +1,12 @@
+import { AppBar } from "@mui/material";
+import { FC } from "react";
+
+const NavBar:FC = () => {
+	return (
+		<AppBar position="static">
+			NavBar Component
+		</AppBar>
+	);
+}
+
+export default NavBar;
