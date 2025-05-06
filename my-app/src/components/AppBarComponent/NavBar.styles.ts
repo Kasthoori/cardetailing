bb4f8daa41c29187ff04252d51a81350f8e73bfc
Kasthoori <ap.kasth@gmail.com>
2025-05-06 18:52:38 +1200

    added menu bar

--- my-app/src/components/AppBarComponent/NavBar.styles.ts
+++ my-app/src/components/AppBarComponent/NavBar.styles.ts
@@ -1,3 +1,6 @@
-export const toolBarClass = 'flex justify-between items-center px-6';
+
+export const toolBarClass = 'flex justify-between items-center px-5 text-white';
 export const titleClass = 'text-white font-bold';
 export const buttonClass = 'text-white hover:text-gray-300';
+export const appBarClass = 'F5F5F5 shadow-md';
+export const typhographyClass = 'text-white !font-light !text-sm sm:!text-base md:!text-lg lg:!text-xl';
--- my-app/src/components/AppBarComponent/NavBar.tsx
+++ my-app/src/components/AppBarComponent/NavBar.tsx
@@ -1,14 +1,78 @@
-import { AppBar, Toolbar } from "@mui/material";
-import { FC } from "react";
-import { toolBarClass } from "./NavBar.styles";
+import { FC, useState } from "react";
+import { toolBarClass, typhographyClass } from "./NavBar.styles";
+import { 
+	AppBar, 
+	Drawer, 
+	IconButton, 
+	List, 
+	ListItem, 
+	ListItemText, 
+	Toolbar,
+	Typography, 
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NavBar:FC = () => {
+
+
+	const [drawerOpen, setDrawerOpen] = useState(false);
+
+	const toggleDrawer = (open: boolean) => () => {
+
+		setDrawerOpen(open);
+	}
+
+	const navItems = ['Home', 'About Us', 'Contact Us'];
+
 	return (
-		<AppBar position="static">
-            <Toolbar className={toolBarClass}>
-			    NavBar Component
-            </Toolbar>
+	
+		<>
+		<AppBar position="static" sx={{ backgroundColor: '#000000' }} className="shadow-md">
+			<Toolbar className={toolBarClass}>
+				<Typography variant="h6" className={typhographyClass}>
+					Car Detailing
+				</Typography>
+				<div className="hidden md:flex space-x-6">
+					{navItems.map((item) => (
+						<a
+							key={item}
+							href="#"
+							className="hover:text-blue-600 transition font-medium"
+						>
+							{item}
+						</a>
+					))}
+				</div>
+				<div className="block md:hidden">
+				<IconButton
+					edge="end"
+					size="large"
+					onClick={toggleDrawer(true)}
+					color="inherit"
+				>
+					<MenuIcon />
+				</IconButton>
+                </div>
+			</Toolbar>
 		</AppBar>
+		
+		<Drawer
+			anchor="right"
+			open={drawerOpen}
+			onClose={toggleDrawer(false)}
+		>
+			<div className="w-64">
+				<List>
+					{navItems.map((item) => (
+						<ListItem component="button" key={item} onClick={toggleDrawer(false)}>
+							<ListItemText primary={item} />
+						</ListItem>
+					))}	
+				</List>
+			</div>
+		</Drawer>
+	 </>
+		
 	);
 }
 
