e614ff07460ca7501197b8c819da5df3e9721954
Kasthoori <ap.kasth@gmail.com>
2025-05-13 17:09:04 +1200

    add image

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react'
 import NavBar from './components/AppBarComponent/NavBar';
+import BannerImage from './components/BannerComponent';
 
 const App:FC = () => {
   return (
     <div>
       <NavBar />
+      <BannerImage />
     </div>
   )
 }
--- my-app/src/components/BannerComponent/BannerImage.tsx
+++ my-app/src/components/BannerComponent/BannerImage.tsx
@@ -1,8 +1,19 @@
 import { FC } from 'react'
+import { Box } from '@mui/material'
 
 const BannerImage:FC = () => {
   return (
     <div>
+      <Box
+        className="w-full"
+        sx={{
+          height: { xs: '200px', sm: '300px', md: '400px', lg: '500px' },
+          backgroundImage: "url('/src/assets/banner-image-cardetailing.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}  
+      />
       
     </div>
   )
