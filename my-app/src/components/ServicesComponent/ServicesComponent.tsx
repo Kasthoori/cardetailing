f2445b6c9938d24cd756c41b9d38930b46b7f3e7
Kasthoori <ap.kasth@gmail.com>
2025-05-15 19:28:19 +1200

    add services component

---
+++ my-app/src/components/ServicesComponent/ServicesComponent.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+const ServicesComponent:FC = () => {
+
+    return (
+        <div className="services-component">            
+            <div className="services-component__title">
+                <h2>Our Services</h2>
+            </div>
+            <div className="services-component__content">
+                <p>We offer a wide range of services to meet your needs.</p>
+            </div>
+        </div>
+    );
+}
+
+export default ServicesComponent;
---
+++ my-app/src/components/ServicesComponent/index.ts
@@ -0,0 +1,2 @@
+export {default} from './ServicesComponent';
+export * from './ServicesComponent';
