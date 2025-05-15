862c00dde02f95855cefb6ee4bc4ac988cd33813
Kasthoori <ap.kasth@gmail.com>
2025-05-15 19:33:21 +1200

    add change to services component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,15 +2,17 @@ import { FC } from 'react'
 import NavBar from './components/AppBarComponent/NavBar';
 import BannerImage from './components/BannerComponent';
 import AboutUsComponent from './components/AboutUsComponent';
+import ServicesComponent from './components/ServicesComponent';
 
 const App:FC = () => {
   return (
     <div>
       <NavBar />
       <BannerImage />
-      <AboutUsComponent />
+      <AboutUsComponent /><br /><br />
+      <ServicesComponent />
     </div>
   )
 }
 
-export default App
+export default App;
--- my-app/src/components/ServicesComponent/ServicesComponent.tsx
+++ my-app/src/components/ServicesComponent/ServicesComponent.tsx
@@ -4,10 +4,10 @@ const ServicesComponent:FC = () => {
 
     return (
         <div className="services-component">            
-            <div className="services-component__title">
+            <div>
                 <h2>Our Services</h2>
             </div>
-            <div className="services-component__content">
+            <div>
                 <p>We offer a wide range of services to meet your needs.</p>
             </div>
         </div>
